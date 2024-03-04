c4444928e41796d1ee76673e158c102eb10de994
cballrun <carter.k.ball@gmail.com>
2024-03-04 12:55:43 -0500

    Create submit_form method, application now able to open webpage and perform a search with results

--- search_item.rb
+++ search_item.rb
@@ -6,6 +6,8 @@ class SearchItem
     include Interactor 
     def call
         fill_form(context.driver, context.wait, context.search_str) #context comes from interactor
+        submit_form(context.driver, context.wait)
+        sleep 2
     end
 
     private 
@@ -15,6 +17,13 @@ class SearchItem
             driver.find_element(:css, "input")
         end
         search_input.send_keys search_str
+    end
+
+    def submit_form(driver, wait)
+        submit_button = wait.until do
+            driver.find_element(:css, "button.nav-search-btn")
+        end
+        submit_button.click
     end
 end
 
