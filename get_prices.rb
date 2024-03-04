c962358bf0466ab52c2de7f4ab02bfee2324e7fe
cballrun <carter.k.ball@gmail.com>
2024-03-04 15:46:00 -0500

    Applcation able to navigate to next page of listings after outputting title/prices of first page of listings

--- get_prices.rb
+++ get_prices.rb
@@ -8,5 +8,13 @@ include Interactor
             context.driver.find_elements(:css, "div.ui-search-result__content")
         end
         puts content_array.map { |listing| listing.find_element(:css, "span.andes-money-amount__fraction").text }
+        next_page_button(context.driver).click
+        sleep 5
     end
+
+        private 
+
+        def next_page_button(driver)
+            driver.find_element(:css, 'li.andes-pagination__button.andes-pagination__button--next')
+        end
 end
